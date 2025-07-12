9532299aa1d22be01ca608d59ad72fe28a4edcb0
NomanDevX <noman404khan@gmail.com>
2025-07-12 12:24:03 +0530

    Add= odd&even

---
+++ odd&even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()  {
+    int n;
+    cout << "Enter the Number : ";
+    cin >> n;
+    
+        if (n%2==0) {
+            cout << "Even\n";
+            
+    }else {
+        cout << "odd\n";
+        
+    }
+        return 0;
+    
+}
