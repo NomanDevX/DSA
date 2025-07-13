e172242cb01b38cd818395d1c4576f37cce58647
NomanDevX <noman404khan@gmail.com>
2025-07-13 13:24:31 +0530

    Loop

---
+++ while_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n=100;
+   int sum=1;
+   while (sum<=n){
+       cout << "sum: " << sum << endl;
+       sum++;
+   }
+    return 0;
+}
